7337093b574bcfc358c30bbe7d08b9ac9aac21e4
Nikita <fomin.nv@phystech.edu>
2024-10-04 11:59:48 +0300

    proccesing start command

---
+++ buttons.py
@@ -0,0 +1,22 @@
+from aiogram import Dispatcher, types
+from aiogram.filters.command import CommandStart
+from aiogram.fsm.storage.memory import MemoryStorage
+from aiogram.fsm.context import FSMContext
+from aiogram.fsm.state import State, StatesGroup
+
+storage = MemoryStorage()
+dp=Dispatcher(storage=storage)
+
+class Form(StatesGroup):
+    answer = State()
+    name: str
+
+@dp.message(CommandStart)
+async def cmd_start(message: types.Message, state: FSMContext):
+    await message.answer('Добрый день! Как вас зовут?')
+    await state.set_state(Form.name)
+
+@dp.message(Form.answer)
+async def answer(message: types.Message, state: FSMContext):
+    name = await state.get_data()['name']
+    await state.clear()
